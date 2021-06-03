c92586b9f309fa40091278aa5f2d2163d661a40c
Marcela <marcelapereirads@gmail.com>
2021-06-03 00:01:02 -0300

    Update status option

--- README.md
+++ README.md
@@ -10,9 +10,10 @@ This application was created as my first nodejs project, with the goal of learni
 * Materialize
 * Nodemon
 
-## How To Use
+## How To Use  (local)
 
-1. Create any database
-2. Rename the .env.example file to .env and fill in the database credentials
-3. Uncomment the sync call on the Post.js file
-4. Run the index.js file with nodemon or node and the tasks table will be created
+1. Clone the project and install dependencies
+2. Create any MySQL database
+3. Rename the .env.example file to .env and fill in the database credentials
+4. Uncomment the sync call on the Post.js file
+5. Run the index.js file with nodemon or node and the tasks table will be created
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const handlebars = require('express-handlebars');
-const Post = require('./models/Post');
+const Task = require('./models/Task');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -12,7 +12,7 @@ app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, resp) => {
-    Post.findAll({ order: [['done', 'ASC']]}).then(posts => resp.render('home', { posts }));
+    Task.findAll({ order: [['done', 'ASC']]}).then(tasks => resp.render('home', { posts: tasks }));
 });
 
 app.get('/new', (req, resp) => resp.render('new'));
@@ -24,15 +24,21 @@ app.post('/new', (req, resp) => {
         done: false
     };
 
-    Post.create(data)
+    Task.create(data)
         .then(() => resp.redirect('/'))
         .catch((error) => console.log('Error saving task:', error));
 });
 
 app.get('/delete/:id', (req, resp) => {
-    Post.destroy({ where: { 'id': req.params.id } })
+    Task.destroy({ where: { 'id': req.params.id } })
         .then(() => resp.redirect('/'))
         .catch((error) => console.log(` Error deleting task id ${req.params.id}: ${error}`));
+});
+
+app.get('/change-status/:id/:status', (req, resp) => {
+    Task.update({ done: req.params.status }, { where: { 'id': req.params.id } })
+        .then(() => resp.redirect('/'))
+        .catch((error) => console.log(` Error updating task id ${req.params.id}: ${error}`));
 });
 
 app.listen(8081, () => console.log('Server listening'));
